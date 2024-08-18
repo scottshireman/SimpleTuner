b119c6b091ca29ea60b5fc68d68a4a3c6aceb3bb
scottshireman <129428073+scottshireman@users.noreply.github.com>
2024-08-18 19:39:16 -0400

    Update docker-start.sh

--- docker-start.sh
+++ docker-start.sh
@@ -33,14 +33,14 @@ if [ -n "$WANDB_TOKEN" ]; then wandb login "$WANDB_TOKEN"; else echo "WANDB_TOKE
 # RunPod JupyterLab
 if [[ $JUPYTER_PASSWORD ]]
 then
-    echo "Starting Jupyter notebooks"
-    jupyter nbextension enable --py widgetsnbextension
-    jupyter labextension disable "@jupyterlab/apputils-extension:announcements"
-    jupyter lab --allow-root --no-browser --port=8888 --ip=* --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace
-    echo "Jupyter started"
+	echo "Starting Jupyter notebooks"
+	jupyter nbextension enable --py widgetsnbextension
+	jupyter labextension disable "@jupyterlab/apputils-extension:announcements"
+	jupyter lab --allow-root --no-browser --port=8888 --ip=* --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace
+	echo "Jupyter started"
 else
-    echo "Container Started"
-    sleep infinity
+	echo "Container Started"
+	sleep infinity
 fi
 
 # 🫡
