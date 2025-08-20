592be81e41bfb066f930551c6da419e0a33f110f
scottshireman <129428073+scottshireman@users.noreply.github.com>
2025-08-20 10:08:56 -0400

    Update docker-start.sh

--- docker-start.sh
+++ docker-start.sh
@@ -48,7 +48,7 @@ if [[ -n "${JUPYTER_PASSWORD:-}" ]]; then
   jupyter nbextension enable --py widgetsnbextension || true
   jupyter labextension disable "@jupyterlab/apputils-extension:announcements" || true
 
-  jupyter lab \
+  exec jupyter lab \
     --allow-root \
     --no-browser \
     --port=8888 \
